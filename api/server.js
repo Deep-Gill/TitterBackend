2bef14e2a769d95c645fccb5e2ca89d5bf4fb851
Deep-Gill <54833651+Deep-Gill@users.noreply.github.com>
2024-08-16 22:39:05 -0700

    Changed file name

--- api/server.js
+++ api/server.js
@@ -4,8 +4,8 @@ const usersRoutes = require('./users.js');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
-app.use(express.json());
-//app.use(bodyParser.json());
+//app.use(express.json());
+app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', usersRoutes);
 //app.use(cookieParser());
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     next();
 });
 
-const allowedOrigins = require('./allowedOrigins')
+const allowedOrigins = require('./allowedOrigins.js')
 
 const corsOptions = {
     origin: (origin, callback) => {
